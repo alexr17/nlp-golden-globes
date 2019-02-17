c4b4f8201e0e5240a705b69a662ad6aaada4e234
Itay Golan <itaygolan19@gmail.com>
2019-02-17 15:13:52 -0600

    add common phrases

--- src/queries/award_names.py
+++ src/queries/award_names.py
@@ -34,9 +34,11 @@ award_kw = ['actor', 'actress', 'supporting']
 answer = {}
 
 # https://github.com/brownrout/EECS-337-Golden-Globes/blob/master/gg_api.py
-def see_awards(data):
+def see_awards(data, bigrams):
     all_awards = {}
+    similar_names = {}
     award_tweets = []
+    bigrams_dict = {}
     for obj in data:
         text = obj['text'].lower()
         if len(set(list_of_awards).intersection(set(text.split(" ")))) > 4:
@@ -60,36 +62,80 @@ def see_awards(data):
                 end = i
 
         temp = []
+        temp1 = []
         #print(tweet[start:end+1])
         for word in tweet[start:end+1]:
+            if word in list_of_awards and word not in helper_words:
+                temp1.append(word)
             if word in list_of_awards or word in helper_words:
                 temp.append(word)
+
         award = ' '.join(sorted(set(temp), key=lambda x: tweet.index(x)))
+        temp1 = ' '.join(set(temp1))
+
         # print(award)
         # print('----')
+
+        if temp1 in similar_names:
+            similar_names[temp1].append(award)
+        else:
+            similar_names[temp1] = [award]
 
         flag = False
         if award in all_awards:
             all_awards[award] += 1
         else:
             all_awards[award] = 1
-        # for x in all_awards:
-        #     if all(word in x for word in set(temp)):
-        #         flag = True
-        # if flag == False:
-        #     all_awards.append(award)
-#
-    #all_awards = [x for x in all_awards if x.split(" ")[0] == 'best']
-    # for x in all_awards:
-    #     x1 = set([a for a in x.split(" ") if a not in helper_words])
-    #     for y in all_awards:
-    #         y1 = set([a for a in y.split(" ") if a not in helper_words])
-    #         if len(x1.intersection(y1)) > 4:
-    #             all_awards.remove(y)
-    award_lst = sorted(all_awards.items(), key=lambda x: x[1], reverse=True)
-    award_lst = [x[0] for x in award_lst if x[0].split(" ")[0] == "best"]
-    return award_lst
 
+        # Find common phrases
+        tokens = sorted(set(temp1.split(" ")), key=lambda x: tweet.index(x))
+        bigrams = [[tokens[i]] + [tokens[i+1]] for i in range(len(tokens)-1)]
+        bigrams = [" ".join(ngram) for ngram in bigrams]
+        for tkn in bigrams:
+            if tkn not in bigrams_dict:
+                 bigrams_dict[tkn] = 1
+            else:
+                 bigrams_dict[tkn] += 1
+
+    bigrams_lst = sorted(bigrams_dict.items(), key=lambda x: x[1], reverse=True)
+
+    common_phrases = [x[0] for x in bigrams_lst[0:15]]
+    print(common_phrases)
+    #print(common_phrases)
+    #common_phrases = ["motion picture", "supporting role", "best performance", "television series", "original score", "original song"]
+
+
+
+    new_award_lst = []
+    for key in similar_names:
+        max_score = 0
+        max_award = ""
+        for award in similar_names[key]:
+            if all_awards[award] > max_score:
+                max_score = all_awards[award]
+                max_award = award
+
+        if any(phrase in max_award for phrase in common_phrases): #.count(True) >= 1:
+            if "supporting" in max_award:
+                if "performance" in max_award:
+                    new_award_lst.append(max_award)
+            else:
+                new_award_lst.append(max_award)
+
+    new_award_lst = [x for x in new_award_lst if x.split(" ")[0] == "best"]
+    threshold = 0.9
+    for i, x in enumerate(new_award_lst):
+        x1 = set([a for a in x.split(" ") if a not in helper_words])
+        for j, y in enumerate(new_award_lst[i+1:]):
+            y1 = set([a for a in y.split(" ") if a not in helper_words])
+            if len(x1.intersection(y1)) > threshold*len(x1):
+                if all_awards[x] > all_awards[y]:
+                    new_award_lst.remove(y)
+                else:
+                    new_award_lst.remove(x)
+                    break
+    print(len(new_award_lst))
+    return new_award_lst
 
 
 def award_set(bigrams):
@@ -124,7 +170,6 @@ def award_set(bigrams):
 
 
 def find_awards(data):
-    return see_awards(data)
     award_dict = {}
 
     best_dict = {
@@ -183,7 +228,7 @@ def find_awards(data):
                 else:
                     award_dict[tkn] += 1
     award_lst = sorted(award_dict.items(), key=lambda x: x[1], reverse=True)
-    return award_set(award_lst)
+    return see_awards(data, award_lst)
     # return award_set([
     #     ("motion picture", 1000),
     #     ("actor motion", 1000),
